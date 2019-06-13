bc0645329ff768a06929ac913a72f49628f6cf74
edwardsun007 <edwardsun007@gmail.com>
2019-06-13 11:55:04 -0700

    Updates

---
+++ precache-manifest.387b288442477781717c68b6a0efbc44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e693524e502d5155f3c678ef18b1e5d8",
+    "url": "/tesla_gif/index.html"
+  },
+  {
+    "revision": "9f02673e5148ad9b1508",
+    "url": "/tesla_gif/static/css/main.abc7ee87.chunk.css"
+  },
+  {
+    "revision": "c382ec47a02b6f9fee43",
+    "url": "/tesla_gif/static/js/2.637d97be.chunk.js"
+  },
+  {
+    "revision": "9f02673e5148ad9b1508",
+    "url": "/tesla_gif/static/js/main.2d3a7589.chunk.js"
+  },
+  {
+    "revision": "9c3bbc230e8e95d9c1eb",
+    "url": "/tesla_gif/static/js/runtime~main.23bf2ed7.js"
+  }
+]);
